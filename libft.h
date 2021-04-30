7fb5b35712b1a1e6b303e769fd5ce1d52c638232
agatocherry <agcolas@student.42.fr>
2021-04-30 16:23:47 +0200

    New functions
    - Add hexalen.c
    - Add puthexa.c
    - Add nblen.c
    - Add putuint.c
    - Update libft.h and makefile

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: agcolas <agcolas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/05 23:51:00 by agcolas           #+#    #+#             */
-/*   Updated: 2021/04/16 11:51:03 by agcolas          ###   ########.fr       */
+/*   Updated: 2021/04/30 16:21:08 by agcolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,23 +24,14 @@ typedef struct	s_list
 }				t_list;
 
 /*
-** libc
+** str
 */
 
-int				ft_atoi(const char *nptr);
-void			ft_bzero(void *s, size_t n);
-void			*ft_calloc(size_t nmemb, size_t size);
 int				ft_isalnum(int c);
 int				ft_isalpha(int c);
 int				ft_isascii(int c);
 int				ft_isdigit(int c);
 int				ft_isprint(int c);
-void			*ft_memccpy(void *dest, const void *src, int c, size_t n);
-void			*ft_memchr(const void *s, int c, size_t n);
-int				ft_memcmp(const void *s1, const void *s2, size_t n);
-void			*ft_memcpy(void *dest, const void *src, size_t n);
-void			*ft_memmove(void *dest, const void *src, size_t n);
-void			*ft_memset(void *s, int c, size_t n);
 char			*ft_strchr(const char *s, int c);
 char			*ft_strdup(const char *s);
 void			ft_putstr(char *str);
@@ -55,25 +46,48 @@ char			*ft_strnstr(const char *s1, const char *s2, size_t len);
 char			*ft_strrchr(const char *s, int c);
 int				ft_tolower(int c);
 int				ft_toupper(int c);
+char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+char			*ft_strtrim(char const *s1, char const *set);
+char			**ft_split(char const *s, char c);
+char			*ft_strjoin(char const *s1, char const *s2);
+void			ft_putuint(unsigned long n);
+void			ft_puthexa(long int nb, char *base);
 
 /*
-** additional function
+** fd
 */
 
-char			*ft_itoa(int n);
-int				ft_antoi(const char *nptr, int len);
 void			ft_putchar_fd(char c, int fd);
 void			ft_putendl_fd(char *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
 void			ft_putstr_fd(char *s, int fd);
-char			**ft_split(char const *s, char c);
-char			*ft_strjoin(char const *s1, char const *s2);
-char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-char			*ft_substr(char const *s, unsigned int start, size_t len);
-char			*ft_strtrim(char const *s1, char const *set);
 
 /*
-** bonus
+** math
+*/
+
+int				ft_atoi(const char *nptr);
+char			*ft_itoa(int n);
+int				ft_antoi(const char *nptr, int len);
+int				ft_nblen(long nb);
+int				ft_hexalen(long int nb);
+
+/*
+** mem
+*/
+
+void			ft_bzero(void *s, size_t n);
+void			*ft_calloc(size_t nmemb, size_t size);
+void			*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void			*ft_memchr(const void *s, int c, size_t n);
+int				ft_memcmp(const void *s1, const void *s2, size_t n);
+void			*ft_memcpy(void *dest, const void *src, size_t n);
+void			*ft_memmove(void *dest, const void *src, size_t n);
+void			*ft_memset(void *s, int c, size_t n);
+
+/*
+** lst
 */
 
 void			ft_lstadd_back(t_list **alst, t_list *new);
